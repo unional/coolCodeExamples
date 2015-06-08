33e62f73c89c69130add3b0a78efec2934f6e402
Homa Wong <hwong@paloaltonetworks.com>
2015-06-08 10:36:18 -0700

    Add export default object literal usage

--- TypeScript/TableTennis/Equipments/BallSize.ts
+++ TypeScript/TableTennis/Equipments/BallSize.ts
@@ -1,11 +1,13 @@
+// define as object literal for demo purpose 
+
 /**
  * Size of the ball, in cm.
  */
-enum BallSize {
-	ThrityEight = 0.38,
-	Fourty = 0.4,
-	FourtyPlus = 0.4025,
-	FourtyFour = 0.44
+var BallSize = {
+	ThrityEight: 0.38,
+	Fourty: 0.4,
+	FourtyPlus: 0.4025,
+	FourtyFour: 0.44
 }
 
 export default BallSize;
